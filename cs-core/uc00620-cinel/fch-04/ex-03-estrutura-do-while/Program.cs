78df528532fa033d29844508df69943d0392494a
F Cesar Farias <fcesarf80@gmail.com>
2026-03-31 20:28:03 +0100

    feat: resolve ex-03 fch 4

---
+++ cs-core/uc00620-cinel/fch-04/ex-03-estrutura-do-while/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Exercicio 03 - Do While
+ * Estrutura repetitiva que testa a condição no final. O bloco é executado sempre uma vez
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_03_estrutura_do_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int op;
+
+            do
+                {
+                  Console.WriteLine("1 - Soma");
+                  Console.WriteLine("2 - Subtração");
+                  Console.WriteLine("3 - Multiplicação");
+                  Console.WriteLine("4 - Divisão");
+                  Console.WriteLine("5 - Sair");
+                  op = Convert.ToInt16(Console.ReadLine());
+
+            } while (op != 0);
+        }
+    }
+}
