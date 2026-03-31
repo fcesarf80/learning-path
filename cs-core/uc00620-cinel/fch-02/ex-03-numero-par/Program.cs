3ae0a22abb4e741bc0173568b6cd014d0f36cfcd
F Cesar Farias <fcesarf80@gmail.com>
2026-03-31 16:43:03 +0100

    feat: resolve ex-03 numero par

--- cs-core/uc00620-cinel/fch-02/ex-03-numero-par/Program.cs
+++ cs-core/uc00620-cinel/fch-02/ex-03-numero-par/Program.cs
@@ -5,10 +5,6 @@
 */
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ex_03_numero_par
 {
@@ -16,6 +12,16 @@ namespace ex_03_numero_par
     {
         static void Main(string[] args)
         {
+            Console.Write("Digite um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("Número par");
+            }
+
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
         }
     }
 }
