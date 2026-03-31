77f51a79f731e179aaff99473ec24f99e1d67f4c
F Cesar Farias <fcesarf80@gmail.com>
2026-03-31 15:55:10 +0100

    feat: resolve ex-11-perimetro-planta

--- cs-core/uc00620-cinel/fch-01/ex-11-perimetro-planta/Program.cs
+++ cs-core/uc00620-cinel/fch-01/ex-11-perimetro-planta/Program.cs
@@ -6,10 +6,6 @@
  */
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ex_11_perimetro_planta
 {
@@ -17,6 +13,32 @@ namespace ex_11_perimetro_planta
     {
         static void Main(string[] args)
         {
+            Console.Write("Digite o valor de A: ");
+            double A = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de B: ");
+            double B = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de R: ");
+            double R = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de Y: ");
+            double Y = double.Parse(Console.ReadLine());
+
+            double perimetroPomar = 2 * (A + B);
+            double areaPomar = A * B;
+            double areaCasa = R * R;
+            double areaPiscina = Math.PI * Math.Pow(Y, 2);
+            double areaJardim = areaPomar - areaCasa - areaPiscina;
+
+            Console.WriteLine($"\nPerímetro do pomar: {perimetroPomar:F2}");
+            Console.WriteLine($"Área do pomar: {areaPomar:F2}");
+            Console.WriteLine($"Área da casa: {areaCasa:F2}");
+            Console.WriteLine($"Área do jardim: {areaJardim:F2}");
+            Console.WriteLine($"Área da piscina: {areaPiscina:F2}");
+
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
         }
     }
 }
