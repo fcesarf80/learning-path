daf36efc6c4dd207edbc4ba64e405441507e75f3
F Cesar Farias <fcesarf80@gmail.com>
2026-03-31 15:31:20 +0100

    feat: resolve ex-07 velocidade media

--- cs-core/uc00620-cinel/fch-01/ex-07-calculo-imposto/Program.cs
+++ cs-core/uc00620-cinel/fch-01/ex-07-calculo-imposto/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * ex - 07 - Cálculo do preço com IVA
+ * Enunciado: Sabendo que a taxa de IVA é de 23%, elabore
+ * um programa em C# que receba o preço de um produto sem
+ * IVA e calcule o preço final com IVA incluído.
+ */
+
+using System;
 
 namespace ex_07_calculo_imposto
 {
@@ -10,6 +13,15 @@ namespace ex_07_calculo_imposto
     {
         static void Main(string[] args)
         {
+            Console.Write("Digite o preço do produto (sem IVA): ");
+            double preco = double.Parse(Console.ReadLine());
+
+            double precoFinal = preco * 1.23;
+
+            Console.WriteLine($"\nPreço com IVA (23%): {precoFinal:F2} €");
+
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
         }
     }
 }
