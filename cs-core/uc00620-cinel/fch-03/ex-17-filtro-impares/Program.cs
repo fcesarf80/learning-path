481f543d50bdd2e1d02040bac13c81b110902084
F Cesar Farias <fcesarf80@gmail.com>
2026-03-31 17:18:57 +0100

    feat: adiciona fichas 1, 2 e 3 completas

--- cs-core/uc00620-cinel/fch-03/ex-17-filtro-impares/Program.cs
+++ cs-core/uc00620-cinel/fch-03/ex-17-filtro-impares/Program.cs
@@ -5,17 +5,22 @@
 */
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ex_17_filtro_impares
+namespace ex_17_continue
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 2 == 0)
+                    continue;
+
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
         }
     }
 }
