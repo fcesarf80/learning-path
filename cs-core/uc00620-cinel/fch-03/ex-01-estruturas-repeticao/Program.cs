5485b37e0ed89fa6c09aba232a37b749fd3d343d
F Cesar Farias <fcesarf80@gmail.com>
2026-03-31 20:29:29 +0100

    Adiciona exercícios de estruturas de repetição e while

---
+++ cs-core/uc00620-cinel/fch-03/ex-01-estruturas-repeticao/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Exercicio 01 Estruturas de repeticao
+ * Estrutura repetitiva com inicio e fim certo
+ * for (inicio do ciclo, fim do ciclo; incremento (algo que permite o ciclo avançar))
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_01_estruturas_repeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int i, num, soma, n;
+            double media;
+            soma = 0;
+            Console.WriteLine("Quantos números quer ler? ");
+            n = Convert.ToInt16(Console.ReadLine());
+
+            for (i = 1; i <= n; i++) {
+
+                Console.Write($"Introduza o {i}º valor: ");
+                num = Convert.ToInt16(Console.ReadLine());
+                soma = soma + num;
+
+            }
+
+            Console.WriteLine(i);
+
+            media = soma / n;
+
+            
+
+            {
+
+            }
+        }
+    }
+}
---
+++ cs-core/uc00620-cinel/fch-04/ex-02-estrutura-while/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Exercício 02 - Estrutura While 
+ * Estrutura utilizada quando não sabemos o fim do ciclo.
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_02_estrutura_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, soma = 0;
+            double media;
+
+            Console.Write("Introduza um valor: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            soma = num;
+
+            while (num != 0)
+            {
+                Console.Write("Introduza um valor: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                soma = soma + num;
+                i = i + 1;
+            }
+
+            media = soma / i;
+
+            Console.WriteLine(i);
+            Console.WriteLine(soma);
+            Console.WriteLine(media);
