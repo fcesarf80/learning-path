6f44deca938ade2696dcfee39c6736ac41be8088
F Cesar Farias <fcesarf80@gmail.com>
2026-04-05 00:59:30 +0100

    feat: inserção e listagem de clientes via JDBC (DAO funcionando)

---
+++ java-core/vetapp/src/vetapp/ClienteDAO.java
@@ -0,0 +1,52 @@
+package vetapp;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class ClienteDAO {
+
+    public void inserir(Cliente cliente) {
+
+        String sql = "INSERT INTO cliente (nome, telefone, email) VALUES (?, ?, ?)";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, cliente.getNome());
+            stmt.setString(2, cliente.getTelefone());
+            stmt.setString(3, cliente.getEmail());
+
+            stmt.executeUpdate();
+            System.out.println("Cliente inserido com sucesso!");
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao inserir cliente:");
+            e.printStackTrace();
+        }
+    }
+
+    public void listar() {
+
+        String sql = "SELECT * FROM cliente";
+
+        try (Connection conn = Conexao.conectar();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                System.out.println("ID: " + rs.getInt("id_cliente"));
+                System.out.println("Nome: " + rs.getString("nome"));
+                System.out.println("Telefone: " + rs.getString("telefone"));
+                System.out.println("Email: " + rs.getString("email"));
+                System.out.println("-----------------------------");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao listar clientes:");
+            e.printStackTrace();
+        }
+    }
+}
--- java-core/vetapp/src/vetapp/Vetapp.java
+++ java-core/vetapp/src/vetapp/Vetapp.java
@@ -1,21 +1,10 @@
 package vetapp;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 public class Vetapp {
 
     public static void main(String[] args) {
 
-        try {
-            Connection conn = Conexao.conectar();
-            System.out.println("Conectado com sucesso ao MySQL!");
-        } catch (SQLException e) {
-            System.out.println("Erro ao conectar:");
-            e.printStackTrace();
-        }
+        ClienteDAO clienteDAO = new ClienteDAO();
+        clienteDAO.listar();
     }
 }
-
-
-
