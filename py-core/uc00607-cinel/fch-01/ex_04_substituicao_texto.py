1af1e29d3f022f3b9d7a52c10b0dd71fffcc1cc2
F Cesar Farias <fcesarf80@gmail.com>
2026-04-13 16:57:15 +0100

    feat: implementa substituição de texto em ficheiro (Ex 04)

--- py-core/uc00607-cinel/fch-01/ex_04_substituicao_texto.py
+++ py-core/uc00607-cinel/fch-01/ex_04_substituicao_texto.py
@@ -3,3 +3,20 @@ Exercício 4 - Substituição texto
 Enunciado: Desenvolva um programa que substitua uma palavra (string) por outra
 num ficheiro. As palavras e o nome do ficheiro deverão ser dados pelo utilizador.
 """
+# 1. Nome do ficheiro pelo utilizador
+nome_ficheiro = input("Nome do ficheiro: ")
+
+with open(nome_ficheiro, "r", encoding="utf-8") as fp:
+    cont = fp.read()
+
+pal_velha = input("Palavra a procurar: ")
+pal_nova = input("Palavra substituta: ")
+
+# 2. Substituir
+novo_cont = cont.replace(pal_velha, pal_nova)
+
+# 3. Gravar as alterações de volta no ficheiro
+with open(nome_ficheiro, "w", encoding="utf-8") as fp:
+    fp.write(novo_cont)
+
+print("Substituição concluída com sucesso!")
