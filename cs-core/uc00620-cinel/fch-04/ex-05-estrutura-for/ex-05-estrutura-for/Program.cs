f51e9b13a87694393276569399eb8a376e565bcf
F Cesar Farias <fcesarf80@gmail.com>
2026-04-14 20:28:15 +0100

    feat: implementa ex-05-vetores-soma

---
+++ cs-core/uc00620-cinel/fch-04/ex-05-estrutura-for/ex-05-estrutura-for/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+* ex-05-estrutura-for
+* Enunciado: "Escreva um programa que leia dois vetores
+* de 3 números e mostre a soma deles."
+*/
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] a = new int[3], b = new int[3], soma = new int[3];
+
+        Console.WriteLine("Valores do vetor A:");
+        for (int i = 0; i < 3; i++) a[i] = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Valores do vetor B:");
+        for (int i = 0; i < 3; i++)
+        {
+            b[i] = int.Parse(Console.ReadLine());
+            soma[i] = a[i] + b[i];
+        }
+
+        Console.WriteLine("Soma: " + string.Join(", ", soma));
+    }
+}
