678974dab00686f8e4fe52af2b6bc00b40d34240
F Cesar Farias <fcesarf80@gmail.com>
2026-04-14 13:27:23 +0100

    refactor: melhoria na lógica de gestão de dados por regiões no ex_07

--- py-core/uc00607-cinel/fch-01/ex_07_gestao_dados_regioes.py
+++ py-core/uc00607-cinel/fch-01/ex_07_gestao_dados_regioes.py
@@ -13,9 +13,9 @@ b. Qual a quantidade de pessoas distribuídas pelas zonas do país (Norte, Centr
 c. Dada a zona do país, determine a média de idades.
 d. Dado o nome de uma pessoa, mostrar onde ela vive (nome da cidade).
    Deve mostrar todas as ocorrências do nome dado.
-e. Dada uma cidade, calcular quantas pessoas lá vivem.
+e. Dada uma cidade, calcular quantas pessoas vivem lá.
 f. Qual a cidade com mais habitantes registados no ficheiro?
-g. Determinar a quantidade de pessoas em todas as cidades e qual a percentagem correspondente.
+g. Determinar a quantidade de pessoas em todas as cidades e qual a porcentagem correspondente.
 h. Dado o nome de uma cidade, perguntar se para a respetiva pessoa, pretende mudar de cidade.
    Se sim, então deve substituir o nome da cidade registado pelo nome da nova cidade.
 i. Crie um novo ficheiro com a informação que está em memória, respeitando o formato do csv.
@@ -83,7 +83,7 @@ print(f"f. Cidade com mais habitantes: {cidade_max}")
 
 # g. Percentagem por cidade
 total = len(dados)
-print("g. Quantidade e Percentagem por cidade:")
+print("g. Quantidade e porcentagem por cidade:")
 for cid, qtd in contagem_cidades.items():
     perc = (qtd/total) * 100
     print(f" - {cid}: {qtd} pessoas ({perc:.2f}%)")
