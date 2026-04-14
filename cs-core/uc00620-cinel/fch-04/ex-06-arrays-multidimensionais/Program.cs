a94b13251a73f5bbcd07bab0fee1da84e074d3d7
F Cesar Farias <fcesarf80@gmail.com>
2026-04-14 21:22:51 +0100

    feat: implementar matriz 3x3 com loops aninhados

---
+++ cs-core/uc00620-cinel/fch-04/ex-06-arrays-multidimensionais/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ex_06_arrays_multidimensionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Declaração da matriz 3x3
+            int[,] matriz = new int[3, 3];
+
+            // 2. Loop para preencher a matriz
+            for (int i = 0; i <= 2; i++)
+            {
+                for (int j = 0; j <= 2; j++)
+                {
+                    Console.Write($"Introduza o valor para a posição {i + 1}, {j + 1}: ");
+                    string entrada = Console.ReadLine();
+                    matriz[i, j] = Convert.ToInt32(entrada);
+                }
+            }
+
+            Console.WriteLine("\n--- Valores Guardados ---");
+
+            // 3. Loop para exibir os valores
+            for (int i = 0; i <= 2; i++)
+            {
+                for (int j = 0; j <= 2; j++)
+                {
+                    Console.WriteLine($"O valor da posição {i + 1}, {j + 1} é: {matriz[i, j]}");
+                }
+            }
+
+            // Mantém a consola aberta até premires uma tecla
+            Console.ReadKey();
+        }
+    }
+}
