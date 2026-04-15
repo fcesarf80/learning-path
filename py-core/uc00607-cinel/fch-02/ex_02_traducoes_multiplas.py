fe2b041ce87dd0b7fe1bada63bc7956596813550
F Cesar Farias <fcesarf80@gmail.com>
2026-04-15 19:24:42 +0100

    Corrige textos e mensagens do exercício 02

--- py-core/uc00607-cinel/fch-02/ex_02_traducoes_multiplas.py
+++ py-core/uc00607-cinel/fch-02/ex_02_traducoes_multiplas.py
@@ -13,7 +13,6 @@ Sugestão: alterar cada valor do dicionário para uma lista/tupla de valores,
 onde o 1º valor é a palavra em inglês (que já lá está) e o 2º valor a palavra
 em francês que irá ser solicitada ao utilizador."""
 
-# --- PARTE 1: Carregar dados do arquivo anterior ---
 dcores = {}
 try:
     with open("dicING.csv", "r", encoding="utf-8") as fp:
@@ -25,15 +24,14 @@ try:
 except FileNotFoundError:
     print("Erro: O arquivo 'dicING.csv' não foi encontrado. Corre o Exercício 01 primeiro.")
     exit()
-# --- PARTE 2: Adicionar as traduções em Francês ---
+
 print("--- Tradução para Francês ---")
 for chave in dcores:
     corING = dcores[chave]
     corFR = input(f"Tradução de «{chave}» para francês? ").strip().title()
-    # Atualiza o dicionário para guardar uma tupla (Inglês, Francês)
     dcores[chave] = (corING, corFR)
-# --- PARTE 3: Guardar no novo arquivo (corPT;corING;corFR) ---
-print("\n--- A gravar novo arquivo 'dicINGFR.csv' ---")
+
+print("\n--- Gravanfo novo arquivo 'dicINGFR.csv' ---")
 with open("dicINGFR.csv", "w", encoding="utf-8") as fp:
     for chave, (ing, fr) in dcores.items():
         fp.write(f"{chave};{ing};{fr}\n")
