2fda994b620b102baf717cd099738a4f7e80e5a9
F Cesar Farias <fcesarf80@gmail.com>
2026-04-15 10:22:05 +0100

    Simplifica estrutura da matriz e remove boilerplate

--- cs-core/uc00620-cinel/fch-04/ex-06-arrays-multidimensionais/Program.cs
+++ cs-core/uc00620-cinel/fch-04/ex-06-arrays-multidimensionais/Program.cs
@@ -11,33 +11,25 @@ namespace ex_06_arrays_multidimensionais
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             int[,] matriz = new int[3, 3];
 
-            // 1. Leitura dos dados
             for (int i = 0; i < 3; i++)
-            {
                 for (int j = 0; j < 3; j++)
                 {
-                    Console.Write($"Valor [{i + 1},{j + 1}]: ");
-                    matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+                    Console.Write($"[{i},{j}]: ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
                 }
-            }
 
-            // 2. Exibição simplificada em formato de matriz
-            Console.WriteLine("\n--- Matriz Resultante ---");
+            Console.WriteLine("\nMatriz:");
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
-                {
-                    // O "\t" alinha os números em colunas
                     Console.Write(matriz[i, j] + "\t");
-                }
-                Console.WriteLine(); // Pula linha após cada linha da matriz
+                Console.WriteLine();
             }
-
-            Console.ReadKey();
         }
     }
 }
+
