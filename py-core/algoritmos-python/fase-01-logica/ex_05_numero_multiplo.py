e51df5cd2c913c61c4e5d002a947dce9cff368ac
F Cesar Farias <fcesarf80@gmail.com>
2026-04-17 17:29:26 +0100

    style: remove redundâncias no script numero_multiplo.py

--- py-core/algoritmos-python/fase-01-logica/ex_05_numero_multiplo.py
+++ py-core/algoritmos-python/fase-01-logica/ex_05_numero_multiplo.py
@@ -1,3 +1,9 @@
 """
 Exercício 05 - Multiplos de cinco?
 """
+num = int(input("Digite um número: "))
+if num % 5 == 0: 
+    res = "é"
+else:
+    res = "não é"
+print(f"{num} {res} multiplo de 5.")
