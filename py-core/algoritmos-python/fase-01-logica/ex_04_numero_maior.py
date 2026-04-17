a6bd8b27f7b718ece7ea5a99a5818bca069c1426
F Cesar Farias <fcesarf80@gmail.com>
2026-04-17 17:21:12 +0100

    style: remove redundâncias no script de ex_04_numero_maior

--- py-core/algoritmos-python/fase-01-logica/ex_04_numero_maior.py
+++ py-core/algoritmos-python/fase-01-logica/ex_04_numero_maior.py
@@ -4,9 +4,8 @@ Exercício 04 - número maior
 n1 = int(input("Digite o 1º número: "))
 n2 = int(input("Digite o 2º número: "))
 if n1 > n2:
-    maior = n1
-    menor = n2
+    print(f"{n1} é maior que {n2}")
+elif n2 > n1:
+    print(f"{n2} é maior que {n1}")
 else:
-    maior = n2
-    menor = n1
-print(f"O número {maior} é maior do que o {menor}")
+    print(f"Os dois números são iguais.")
