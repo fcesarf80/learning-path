55ac0cd9cb0573311f9f8204a174cbe4e019705f
F Cesar Farias <fcesarf80@gmail.com>
2026-04-29 17:37:28 +0100

    refactor: melhoria no ex_02_gestao_notas_alunos.py

--- py-core/uc00607-cinel/fch-04/ex_02_gestao_notas_alunos.py
+++ py-core/uc00607-cinel/fch-04/ex_02_gestao_notas_alunos.py
@@ -3,22 +3,8 @@ Exercício 02 - Gestão notas alunos
 Enunciado: Cria um dicionário com 5 alunos e respetivas notas (entre 0 e 20) e:
 • Mostra todos os alunos • Calcula a média das notas • Listagem dos alunos com nota positiva (>= 10)
 """
-alunos_notas = {
-    "Ana": 15, "Bruno": 8, "Carlos": 12, "Diana": 18, "Eduardo": 9
-    }
-soma = 0
-total_alunos = 0
-
-print("Lista de todos os alunos:")
-for nome in alunos_notas:
-    nota = alunos_notas[nome]
-    print(nome, "-", nota)
-    soma += nota
-    total_alunos += 1
-media = soma / total_alunos
-print("\nMédia das notas:", media)
-
-print("\nAlunos com nota positiva:")
-for nome in alunos_notas:
-    if alunos_notas[nome] >= 10:
-        print(nome)
+alunos = {"Cuca": 15, "Iara": 8, "Boto": 12, "Saci": 18, "Tutu": 9}
+print("Lista de alunos e notas:\n" + "\n".join([f"{n} - {v}" for n, v in alunos.items()]))
+print(f"\nMédia das notas: {sum(alunos.values()) / len(alunos)}")
+positivos = [f"{n} - {v}" for n, v in alunos.items() if v >= 10]
+print("\nAlunos com nota positiva (>= 10):\n" + "\n".join(positivos))
