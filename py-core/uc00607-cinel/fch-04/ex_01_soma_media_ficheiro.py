34a8403d97d2ad4c6acf2943626f73f178f4b7af
F Cesar Farias <fcesarf80@gmail.com>
2026-04-29 11:49:06 +0100

    refactor: melhoria no ex_01_gestão_ficheiro.py

--- py-core/uc00607-cinel/fch-04/ex_01_soma_media_ficheiro.py
+++ py-core/uc00607-cinel/fch-04/ex_01_soma_media_ficheiro.py
@@ -1,21 +1,24 @@
 """
 Exercício 01 - Soma media ficheiro
-Enunciado: Cria um programa que:
-• Leia um ficheiro numeros.txt (1 número por cada linha do ficheiro)
-• Some todos os números
+Enunciado: Cria um programa que: • Leia um ficheiro numeros.txt
+(1 número por cada linha do ficheiro) • Some todos os números
 • Mostre o total da soma e apresente a média desses valores no ecrã
 """
-ficheiro = open("numeros.txt", "r")
-soma = 0
-quantidade = 0
-for linha in ficheiro:
-    numero = float(linha)    
-    soma = soma + numero
-    quantidade += 1
+ficheiro = open("numeros.txt", "w")
+ficheiro.write("10\n")
+ficheiro.write("20\n")
+ficheiro.write("30\n")
+ficheiro.write("40\n")
+ficheiro.write("50\n")
 ficheiro.close()
-if quantidade > 0:
-    media = soma / quantidade
-    print("Total da soma:", soma)
-    print("Média dos valores:", media)
-else:
-    print("O ficheiro está vazio.")
+soma = 0
+quantidade_numeros = 0
+ficheiro = open("numeros.txt", "r")
+for linha in ficheiro:
+    numero = int(linha)
+    soma = soma + numero
+    quantidade_numeros = quantidade_numeros + 1
+ficheiro.close()
+media = soma / quantidade_numeros
+print("Soma total:", soma)
+print("Média:", media)
