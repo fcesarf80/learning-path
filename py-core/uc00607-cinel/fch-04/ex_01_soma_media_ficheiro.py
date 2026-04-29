b07f17d7664e9ac7ead225602fbaf38c94068f65
F Cesar Farias <fcesarf80@gmail.com>
2026-04-29 12:15:34 +0100

    refactor: melhoria no ex_01_gestão_ficheiro.py

--- py-core/uc00607-cinel/fch-04/ex_01_soma_media_ficheiro.py
+++ py-core/uc00607-cinel/fch-04/ex_01_soma_media_ficheiro.py
@@ -4,21 +4,11 @@ Enunciado: Cria um programa que: • Leia um ficheiro numeros.txt
 (1 número por cada linha do ficheiro) • Some todos os números
 • Mostre o total da soma e apresente a média desses valores no ecrã
 """
-ficheiro = open("numeros.txt", "w")
-ficheiro.write("10\n")
-ficheiro.write("20\n")
-ficheiro.write("30\n")
-ficheiro.write("40\n")
-ficheiro.write("50\n")
-ficheiro.close()
-soma = 0
-quantidade_numeros = 0
-ficheiro = open("numeros.txt", "r")
-for linha in ficheiro:
-    numero = int(linha)
-    soma = soma + numero
-    quantidade_numeros = quantidade_numeros + 1
-ficheiro.close()
-media = soma / quantidade_numeros
-print("Soma total:", soma)
-print("Média:", media)
+with open("numeros.txt", "w") as f:
+    f.write("10\n20\n30\n40\n50\n")
+with open("numeros.txt", "r") as f:
+    numeros = [int(linha) for linha in f]
+soma = sum(numeros)
+quantidade = len(numeros)
+media = soma / quantidade
+print(f"Soma total: {soma} | Média: {media}")
