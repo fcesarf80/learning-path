fa43449ef61637f033716f37804b6d0776daed1a
F Cesar Farias <fcesarf80@gmail.com>
2026-05-10 17:50:00 +0100

    Add: ex_02_listar_alunos.py

--- py-core/uc00608-cinel/fch-01/ex_02_lista_alunos.py
+++ py-core/uc00608-cinel/fch-01/ex_02_lista_alunos.py
@@ -4,3 +4,25 @@ Enunciado: Atualize o código anterior para carregar informação de n dados de
 alunos. Armazene a informação numa lista. Em seguida, apresente os valores 
 de cada aluno.
 """
+class Aluno:
+    def __init__(self, nome, sobrenome, numero, curso):
+        self.nome, self.sobrenome = nome, sobrenome
+        self.numero, self.curso = numero, curso    
+    def executar(self):
+        print(f"Nome: {self.nome} {self.sobrenome} | "
+              f"Nº: {self.numero} | Curso: {self.curso}")        
+lista_alunos = []
+quantidade_alunos = int(input("Quantos alunos deseja cadastrar? "))
+for contador in range(quantidade_alunos):
+    print(f"\n--- Cadastro do Aluno {contador + 1} ---")    
+    dados_nome = input("Digite o Nome e Sobrenome: ").split()
+    nome = dados_nome[0]
+    sobrenome = " ".join(dados_nome[1:])
+    dados_extras = input("Digite o Nº e o Curso: ").split()
+    numero = dados_extras[0]
+    curso = " ".join(dados_extras[1:])    
+    novo_aluno = Aluno(nome, sobrenome, numero, curso)
+    lista_alunos.append(novo_aluno)
+print("\n--- Lista Final de Alunos ---")
+for aluno in lista_alunos:
+    aluno.executar()
