821e7eb5d32a5016fb8ea33e88e230c8160b9a21
F Cesar Farias <fcesarf80@gmail.com>
2026-05-28 20:27:12 +0100

    Padroniza a indentação do código

--- py-core/uc00608-cinel/fch-01/ex_01_classe_aluno.py
+++ py-core/uc00608-cinel/fch-01/ex_01_classe_aluno.py
@@ -3,12 +3,19 @@ Exercicio 01 - Classe Aluno
 Enunciado: Desenvolva a classe Aluno com as propriedades nome,
 sobrenome, número e curso, e um método para a visualização dos dados.
 """
+
+
 class Aluno:
     def __init__(self, nome, sobrenome, numero, curso):
         self.nome, self.sobrenome = nome, sobrenome
         self.numero, self.curso = numero, curso
+
     def executar(self):
-        print(f"Nome: {self.nome} {self.sobrenome} | "
-              f"Nº: {self.numero} | Curso: {self.curso}")
+        print(
+            f"Nome: {self.nome} {self.sobrenome} | "
+            f"Nº: {self.numero} | Curso: {self.curso}"
+        )
+
+
 aluno_01 = Aluno("Pato", "Donald", 313, "Marujo")
 aluno_01.executar()
