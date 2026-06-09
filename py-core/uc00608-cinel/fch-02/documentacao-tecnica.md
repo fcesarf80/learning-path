a158c95c3126e9f0c16448ce428edbf613483986
F Cesar Farias <fcesarf80@gmail.com>
2026-06-09 21:47:31 +0100

    Implementada pesquisa de alunos por nomecls

---
+++ py-core/uc00608-cinel/fch-02/documentacao-tecnica.md
@@ -0,0 +1,76 @@
+# Documentação Técnica
+
+Curso: Técnico Especialista em Tecnologias e Programação de Sistemas de Informação
+UC: 00608 - Desenvolver programas em linguagem orientada a objetos
+
+Formador: Júlio Guilherme Moura Magalhães
+Formando: Fernando Cesar Ferreira Farias (Cesar)
+Projeto: ex_04_gestor_alunos.py
+
+---
+
+# Histórico de Desenvolvimento
+
+## Etapa 1 - Criação da Classe Aluno
+
+### Objetivo
+
+Criar a classe Aluno e definir os atributos necessários para o sistema.
+
+### Conteúdos Trabalhados
+
+* Classes
+* Objetos
+* Método construtor (**init**)
+* Atributos de instância
+
+### Aprendizagem
+
+...
+
+---
+
+## Etapa 1A - Expansão da Classe Aluno
+
+### Objetivo
+
+Expandir a modelagem da classe para incluir informações adicionais.
+
+### Novos atributos
+
+* numero
+* sobrenome
+* curso
+
+### Aprendizagem
+
+...
+
+---
+
+# Registros de Decisão de Projeto
+
+## RDP-001 - Turma da Edna Krabappel
+
+### Contexto
+
+Foi necessário definir uma turma de referência para os testes do sistema.
+
+### Decisão
+
+Utilizar exclusivamente os alunos da turma da professora Edna Krabappel (The Simpsons).
+
+### Justificativa
+
+* Maior consistência nos testes.
+* Dados mais interessantes que exemplos genéricos.
+* Facilita validações e demonstrações.
+
+### Impacto
+
+Apenas os dados de teste foram alterados.
+Os requisitos do enunciado permanecem inalterados.
+
+### Estado
+
+Aprovado.
--- py-core/uc00608-cinel/fch-02/ex_04_gestor_alunos.py
+++ py-core/uc00608-cinel/fch-02/ex_04_gestor_alunos.py
@@ -5,12 +5,77 @@ a. Adicionar um novo aluno          d. Listar alunos ordenados pela maior média
 b. Listar todos os alunos           e. Remover aluno
 c. Procurar um aluno pelo nome      f. Sair do programa
 """
+
 class Aluno:
 
     def __init__(self, numero, nome, sobrenome, idade, curso, media):
-        self.nome, self.sobrenome = nome, sobrenome
-        self.idade, self.numero = idade, numero
-        self.curso, self.media = curso, media
-    
-print("Programa iniciado")
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.idade = idade
+        self.numero = numero
+        self.curso = curso
+        self.media = media
+
+lista_alunos = []
+
+lista_alunos.append(
+    Aluno(1, "Bart", "Simpson", 10, "4A - Edna Krabappel", 11.0)
+)
+
+lista_alunos.append(
+    Aluno(2, "Milhouse", "Van Houten", 10, "4A - Edna Krabappel", 13.0)
+)
+
+lista_alunos.append(
+    Aluno(3, "Nelson", "Muntz", 10, "4A - Edna Krabappel", 9.0)
+)
+
+lista_alunos.append(
+    Aluno(4, "Martin", "Prince", 10, "4A - Edna Krabappel", 19.0)
+)
+
+lista_alunos.append(
+    Aluno(5, "Ralph", "Wiggum", 10, "4A - Edna Krabappel", 7.0)
+)
+
+lista_alunos.append(
+    Aluno(6, "Sherri", "Mackleberry", 10, "4A - Edna Krabappel", 15.0)
+)
+
+lista_alunos.append(
+    Aluno(7, "Terri", "Mackleberry", 10, "4A - Edna Krabappel", 15.0)
+)
+
+lista_alunos.append(
+    Aluno(8, "Wendell", "Borton", 10, "4A - Edna Krabappel", 12.0)
+)
+
+lista_alunos.append(
+    Aluno(9, "Lewis", "Clark", 10, "4A - Edna Krabappel", 14.0)
+)
+
+lista_alunos.append(
+    Aluno(10, "Richard", "", 10, "4A - Edna Krabappel", 13.0)
+)
+
+lista_alunos.append(
+    Aluno(11, "Nina", "Skalka", 10, "4A - Edna Krabappel", 16.0)
+)
+
+lista_alunos.append(
+    Aluno(12, "Sophie", "Jensen", 10, "4A - Edna Krabappel", 17.0)
+)
+
+lista_alunos.append(
+    Aluno(13, "Becky", "Shorter", 10, "4A - Edna Krabappel", 14.0)
+)
+
+print("\n=== TURMA DA EDNA KRABAPPEL ===\n")
+
+for aluno in lista_alunos:
+    print(
+        f"{aluno.numero:02d} - "
+        f"{aluno.nome} {aluno.sobrenome}"
+    )
+
         
