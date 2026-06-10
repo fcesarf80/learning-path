56164859d862e3da6f2077abbea4bacd5f559dad
F Cesar Farias <fcesarf80@gmail.com>
2026-06-10 21:54:49 +0100

    Adiciona background da interface Tkinter

--- py-core/uc00608-cinel/fch-02/main.py
+++ py-core/uc00608-cinel/fch-02/main.py
@@ -1,16 +1,53 @@
 import tkinter as tk
 
+from PIL import Image
+from PIL import ImageTk
+
+
+# ==========================
+# JANELA
+# ==========================
+
 janela = tk.Tk()
 
-janela.title("Turma da Edna Krabappel")
+janela.title("Gestor de Alunos - Turma da Edna")
 janela.geometry("1366x768")
+janela.resizable(False, False)
 
-label = tk.Label(
-    janela,
-    text="Tkinter funcionando!",
-    font=("Arial", 24)
+
+# ==========================
+# BACKGROUND
+# ==========================
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+
+caminho_fundo = BASE_DIR / "img" / "tela" / "background.png"
+
+imagem_fundo = Image.open(caminho_fundo)
+
+imagem_fundo = imagem_fundo.resize(
+    (1366, 768)
 )
 
-label.pack(pady=50)
+fundo = ImageTk.PhotoImage(imagem_fundo)
+
+label_fundo = tk.Label(
+    janela,
+    image=fundo
+)
+
+label_fundo.place(
+    x=0,
+    y=0,
+    relwidth=1,
+    relheight=1
+)
+
+
+# ==========================
+# LOOP
+# ==========================
 
 janela.mainloop()
