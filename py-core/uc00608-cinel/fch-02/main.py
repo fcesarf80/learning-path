58ba34870ba889d3fa337b6b406d21ad0b15a2ee
F Cesar Farias <fcesarf80@gmail.com>
2026-06-16 20:01:00 +0100

    Adicionados Treeview, painel de funcionalidades e organização da interfacecls

--- py-core/uc00608-cinel/fch-02/main.py
+++ py-core/uc00608-cinel/fch-02/main.py
@@ -4,6 +4,7 @@
 
 import tkinter as tk
 
+from tkinter import ttk
 from tkinter import filedialog
 from pathlib import Path
 from PIL import Image
@@ -16,6 +17,49 @@ from services.gestor_alunos import lista_alunos
 
 aluno_atual = lista_alunos[0]
 indice_aluno = 0
+
+# ==================================================
+# CAMINHOS DAS IMAGENS
+# ==================================================
+
+BASE_DIR = Path(__file__).parent
+
+caminho_fundo = (
+    BASE_DIR
+    / "img"
+    / "tela"
+    / "background.png"
+)
+
+caminho_foto_vazia = (
+    BASE_DIR
+    / "img"
+    / "tela"
+    / "foto_vazia.png"
+)
+
+caminho_seta_esquerda = (
+    BASE_DIR
+    / "img"
+    / "tela"
+    / "mao-indicador-esquerda.png"
+)
+
+caminho_seta_direita = (
+    BASE_DIR
+    / "img"
+    / "tela"
+    / "mao-indicador-direita.png"
+)
+
+caminho_camera = (
+    BASE_DIR
+    / "img"
+    / "tela"
+    / "maquina_fotografica.png"
+)
+
+# MONTA O CAMINHO DA FOTO
 
 # ==================================================
 # FUNÇÕES DA INTERFACE
@@ -105,8 +149,8 @@ def mostrar_aluno():
 
     label_media.config(
     text=f"Média: {aluno.media}"
-    )    
-
+    )
+  
     # ATUALIZA A FOTO
 
     imagem = Image.open(caminho_foto)
@@ -118,6 +162,22 @@ def mostrar_aluno():
     )
     
     label_foto.image = foto_aluno
+
+def listar_alunos():
+
+    for aluno in lista_alunos:
+
+        tree_alunos.insert(
+            "",
+            "end",
+            values=(
+                aluno.numero,
+                aluno.nome,
+                aluno.sobrenome,
+                aluno.idade,
+                aluno.media
+            )
+        )
 
 # ==================================================
 # JANELA PRINCIPAL
@@ -133,17 +193,17 @@ janela.resizable(False, False)
 # BACKGROUND
 # ==================================================
 
-BASE_DIR = Path(__file__).parent
-
-caminho_fundo = BASE_DIR / "img" / "tela" / "background.png"
-
-imagem_fundo = Image.open(caminho_fundo)
+imagem_fundo = Image.open(
+    caminho_fundo
+)
 
 imagem_fundo = imagem_fundo.resize(
     (1112, 743)
 )
 
-fundo = ImageTk.PhotoImage(imagem_fundo)
+fundo = ImageTk.PhotoImage(
+    imagem_fundo
+)
 
 label_fundo = tk.Label(
     janela,
@@ -210,7 +270,7 @@ label_foto.place(
 label_nome = tk.Label(
     janela,
     text="",
-    font=("Arial", 12, "bold"),
+    font=("Fredoka SemiBold", 12, "bold"),
     bg="#fee686",
     fg="#114510",
     width=15
@@ -218,7 +278,7 @@ label_nome = tk.Label(
 
 label_nome.place(
     x=220,
-    y=505
+    y=475
 )
 
 # NÚMERO DO ALUNO
@@ -226,12 +286,14 @@ label_nome.place(
 label_numero = tk.Label(
     janela,
     text="",
-    bg="white"
+    font=("Fredoka semibold", 10),
+    bg="#fee686",
+    fg="#114510"
 )
 
 label_numero.place(
     x=220,
-    y=500
+    y=525
 )
 
 # IDADE DO ALUNO
@@ -239,11 +301,13 @@ label_numero.place(
 label_idade = tk.Label(
     janela,
     text="",
-    bg="white"
+    font=("Fredoka semibold", 10),
+    bg="#fee686",
+    fg="#114510"
 )
 
 label_idade.place(
-    x=220,
+    x=255,
     y=525
 )
 
@@ -252,7 +316,9 @@ label_idade.place(
 label_curso = tk.Label(
     janela,
     text="",
-    bg="white"
+    font=("Fredoka semibold", 10),
+    bg="#fee686",
+    fg="#114510"
 )
 
 label_curso.place(
@@ -265,12 +331,54 @@ label_curso.place(
 label_media = tk.Label(
     janela,
     text="",
-    bg="white"
+    font=("Fredoka semibold", 10),
+    bg="#fee686",
+    fg="#114510"
 )
 
 label_media.place(
-    x=220,
-    y=575
+    x=313,
+    y=525
+)
+
+# ==================================================
+# ÍCONES
+# ==================================================
+
+imagem_seta_esquerda = Image.open(
+    caminho_seta_esquerda
+)
+
+imagem_seta_esquerda = imagem_seta_esquerda.resize(
+    (36, 36)
+)
+
+icone_seta_esquerda = ImageTk.PhotoImage(
+    imagem_seta_esquerda
+)
+
+imagem_seta_direita = Image.open(
+    caminho_seta_direita
+)
+
+imagem_seta_direita = imagem_seta_direita.resize(
+    (36, 36)
+)
+
+icone_seta_direita = ImageTk.PhotoImage(
+    imagem_seta_direita
+)
+
+imagem_camera = Image.open(
+    caminho_camera
+)
+
+imagem_camera = imagem_camera.resize(
+    (36, 36)
+)
+
+icone_camera = ImageTk.PhotoImage(
+    imagem_camera
 )
 
 # ==================================================
@@ -279,54 +387,138 @@ label_media.place(
 
 # BOTÃO CARREGAR FOTO
 
-botao_teste = tk.Button(
+botao_carregar_foto = tk.Button(
     janela,
-    text="CARREGAR FOTO",
-    command=carregar_foto
+    image=icone_camera,
+    command=carregar_foto,
+    bg="#fee686",
+    bd=0,
+    highlightthickness=0
 )
 
-botao_teste.place(
-    x=240,
-    y=480
+botao_carregar_foto.place(
+    x=270,
+    y=493
 )
 
 # BOTÃO ALUNO ANTERIOR
 
 botao_anterior = tk.Button(
     janela,
-    text="←"
-)
-
-botao_anterior.config(
-    command=aluno_anterior
+    image=icone_seta_esquerda,
+    command=aluno_anterior,
+    bg="#fee686",
+    bd=0,
+    highlightthickness=0
 )
 
 botao_anterior.place(
-    x=220,
-    y=520
+    x=210,
+    y=493
 )
 
 # BOTÃO PRÓXIMO ALUNO
 
 botao_proximo = tk.Button(
     janela,
-    text="→"
-)
-
-botao_proximo.config(
-    command=proximo_aluno
+    image=icone_seta_direita,
+    command=proximo_aluno,
+    bg="#fee686",
+    bd=0,
+    highlightthickness=0
 )
 
 botao_proximo.place(
-    x=320,
-    y=520
+    x=330,
+    y=493
 )
+
+# ==================================================
+# GRELHA DE ALUNOS
+# ==================================================
+
+tree_alunos = ttk.Treeview(
+    janela,
+    columns=(
+        "numero",
+        "nome",
+        "sobrenome",
+        "idade",
+        "media"
+    ),
+    show="headings"
+)
+
+tree_alunos.place(
+    x=421,
+    y=470,
+    width=700,
+    height=150
+)
+
+# CABEÇALHO
+
+tree_alunos.heading(
+    "numero",
+    text="Nº"
+)
+
+tree_alunos.heading(
+    "nome",
+    text="Nome"
+)
+
+tree_alunos.heading(
+    "sobrenome",
+    text="Sobrenome"
+)
+
+tree_alunos.heading(
+    "idade",
+    text="Idade"
+)
+
+tree_alunos.heading(
+    "media",
+    text="Média"
+)
+
+# LARGURAS
+
+tree_alunos.column(
+    "numero",
+    width=50
+)
+
+tree_alunos.column(
+    "nome",
+    width=180
+)
+
+tree_alunos.column(
+    "sobrenome",
+    width=180
+)
+
+tree_alunos.column(
+    "idade",
+    width=80
+)
+
+tree_alunos.column(
+    "media",
+    width=80
+)
+
+# REGISTROS
 
 # ==================================================
 # INICIALIZAÇÃO
 # ==================================================
 
 mostrar_aluno()
+
+listar_alunos()
 
 # ==================================================
 # LOOP PRINCIPAL
